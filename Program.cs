e79be05e071b3c97b350514a0525d5657042e014
180892PauloCesar <paulo91.net@gmail.com>
2020-05-01 23:17:19 -0300

    Check-in inicial do módulo Somar

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Somar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número:");
+            decimal numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro número:");
+            decimal numero2 = int.Parse(Console.ReadLine());
+            decimal soma = numero1 + numero2;
+            Console.WriteLine("A soma dos valores digitados " + numero1 + " e " + numero2 + " é: " + soma);
+        }
+    }
+}
